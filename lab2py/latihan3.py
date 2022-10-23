f533c3b387791dc24d97be094f5990503ff03585
Fadilah <fadilahadeliani23@gmail.com>
2022-10-23 15:33:14 +0700

    menambahkan lab2py

---
+++ lab2py/latihan3.py
@@ -0,0 +1,26 @@
+# input nilai variable
+a=input("Masukan nilai a=")
+b=input("Masukan nilai b=")
+
+# cetak nilai variable
+print ("Variable a=",a)
+print ("Variable b=",b)
+
+# cetak kedua variable dengan format string
+format_string = f" hasil penggabungan a dan b adalah ={a}{b}"
+
+print(format_string)
+
+# Konversi variable
+a=int(a)
+b=int(b)
+
+format_jumlah = f" hasil penjumlahan a dan b adalah ={a+b}"
+
+print(format_jumlah)
+
+format_bagi = f" hasil pembagian a dan b adalah ={a/b}"
+
+print(format_bagi)
+
+
